5e1a9728de02c291948bae608bc0a38efaf14891
Marcel Dao <marcelnamdao@gmail.com>
2024-12-02 01:23:54 -0500

    Add missing colon in Connecting database url

--- src/teamb/Connecting.java
+++ src/teamb/Connecting.java
@@ -12,7 +12,7 @@ import java.sql.*;
 public class Connecting {
     public static Connection letConnect() {
         Connection conn = null;
-        String url = "jdbc:derbyMovieTheaterDB";
+        String url = "jdbc:derby:MovieTheaterDB";
         
         try{Class.forName("org.apache.derby.jdbc.EmbeddedDriver");
                 conn = DriverManager.getConnection(url);
